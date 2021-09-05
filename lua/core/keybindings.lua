c5e72d5f105c7c9f3a5f23c304e151ce3a470987
Jerome <josejeromelalunio@gmail.com>
2021-09-05 10:08:57 +0800

    add configure bufferline

--- lua/core/keybindings.lua
+++ lua/core/keybindings.lua
@@ -11,17 +11,16 @@ keymap('n', '<leader>/', ':noh <cr>', options);
 keymap('n', '<leader>v', ':vsplit <cr>', options);
 
 -- navigation through buffers
-keymap('n', '<C-h>', '<C-W>h', options);
-keymap('n', '<C-j>', '<C-W>j', options);
-keymap('n', '<C-k>', '<C-W>k', options);
-keymap('n', '<C-l>', '<C-W>l', options);
+keymap('n', '<s-h>', '<C-W>h', options);
+keymap('n', '<s-n>', '<C-W>j', options);
+keymap('n', '<s-k>', '<C-W>k', options);
+keymap('n', '<s-l>', '<C-W>l', options);
 
 -- telescope
-
 keymap('n', '<leader>ff', '<cmd> Telescope find_files <cr>', options);
 keymap('n', '<leader>fg', '<cmd> Telescope live_grep <cr>', options);
 keymap('n', '<leader>fb', '<cmd> Telescope buffers <cr>', options);
 keymap('n', '<leader>fh', '<cmd> Telescope help_tags <cr>', options);
 
 -- lsp
-keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', options);
+keymap('n', 'gh', '<cmd>lua vim.lsp.buf.hover()<CR>', options);
