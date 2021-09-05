a9950e5858f5870e692883b5abb11bd5a3537539
Jerome <josejeromelalunio@gmail.com>
2021-09-05 10:47:42 +0800

    configure galaxyline for git

--- lua/package-configs/galaxyline.lua
+++ lua/package-configs/galaxyline.lua
@@ -5,15 +5,15 @@ local myColors = {white = '#ffffff', orange = '#ff8000', bg = '#4c566a', lightGr
 
 component.left[2] = {Error = {provider = 'DiagnosticError', icon = '  ', highlight = {myColors.white, colors.red}}}
 component.left[3] = {Warn = {provider = 'DiagnosticWarn', icon = '  ', highlight = {myColors.white, colors.yellow}}}
-component.left[4] = {Hint = {provider = 'DiagnosticHint', icon = '  ', highlight = {myColors.white, colors.blue}}}
+component.left[4] = {Hint = {provider = 'DiagnosticHint', icon = '  ', highlight = {myColors.white, colors.blue}}}
 component.left[6] = {Info = {provider = 'DiagnosticInfo', icon = '  ', highlight = {myColors.white, colors.blue}}}
 component.left[7] = {
     Line = {
         provider = 'LinePercent',
         icon = '  ',
-        highlight = {myColors.white, colors.blue},
+        highlight = {myColors.white, colors.orange},
         separator = '',
-        separator_highlight = {colors.blue, myColors.bg}
+        separator_highlight = {colors.orange, myColors.bg}
     }
 }
 
@@ -34,5 +34,33 @@ component.right[1] = {
         highlight = {myColors.white, myColors.orange},
         separator = '',
         separator_highlight = {myColors.orange, myColors.bg}
+    }
+}
+
+component.right[2] = {
+    DiffAdd = {
+        provider = 'DiffAdd',
+        icon = '  ',
+        highlight = {myColors.white, colors.green},
+        separator = ' ',
+        separator_highlight = {myColors.bg, colors.green}
+    }
+}
+component.right[3] = {
+    DiffModified = {
+        provider = 'DiffModified',
+        icon = '  ',
+        highlight = {myColors.white, colors.blue},
+        separator = ' ',
+        separator_highlight = {myColors.bg, colors.blue}
+    }
+}
+component.right[4] = {
+    DiffRemove = {
+        provider = 'DiffRemove',
+        icon = '  ',
+        highlight = {myColors.white, colors.red},
+        separator = ' ',
+        separator_highlight = {myColors.bg, colors.red}
     }
 }
